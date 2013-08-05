9e04be6109613368f2b13ef749dddcfac765e668
Jack Dent <jack@jackdent.co.uk>
2013-08-05 15:33:06 +0100

    Migration to new build structure

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -1,5 +1,30 @@
 moduleScripts['dashboard'] = {
-  'notifications' : {},
-  'groups' : {},
-  'deadlines' : {}
+	'notifications' : {
+		'getNotificationsController': [
+			bindNotificationShowMoreListener
+		]
+	},
+	'groups' : {
+		'index': [
+			groupsIndex
+		]
+	},
+	'deadlines' : {
+		'index' : [
+			deadlinesIndex
+		],
+		'edit' : [
+			applyDatepicker
+		]
+	},
+	'supervisor' : {
+		'index' : [
+	        editDeadline,
+	        deleteDeadline,
+	        deadlineAutocomplete,
+	        createGroup,
+	        deleteGroup,
+	        applyDatepicker
+		]	
+	}
 }
