289629526791501c0b33b6165b4c7de153c25037
Jack Dent <jack@jackdent.co.uk>
2013-08-22 17:39:20 +0100

    New deadline prepopulated URL field

--- src/main/java/uk/ac/cam/dashboard/controllers/SupervisorController.java
+++ src/main/java/uk/ac/cam/dashboard/controllers/SupervisorController.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
@@ -25,18 +26,29 @@ public class SupervisorController extends ApplicationController {
 	// Index 
 	@GET @Path("/")
 	public ImmutableMap<String, ?> indexSupervisor() {
-		return indexSupervisorTab("");
+		return indexSupervisorTab("", "");
 	}
 	
 	@GET @Path("/{tab}") 
-	public ImmutableMap<String, ?> indexSupervisorTab(@PathParam("tab") String tab) {
+	public ImmutableMap<String, ?> indexSupervisorTab(@PathParam("tab") String tab, @QueryParam("url") String url) {
 
 		currentUser = getUser();
+		
+		url = (url == null ? "" : url);
 		
 		if(currentUser.getSupervisor()){
 			log.debug("User authorised, returning group and deadline management data JSON");
 			tab = (tab == null || tab == "" ? "undefined" : tab);
-			return ImmutableMap.of("user", currentUser.toMap(), "target", tab, "cdeadlines", currentUser.createdDeadlinesToMap(), "cgroups", currentUser.groupsToMap(), "errors", "undefined");
+			
+			ImmutableMap.Builder<String, Object> builder = new ImmutableMap.Builder<String, Object>();
+			builder.put("user", currentUser.toMap());
+			builder.put("target", tab);
+			builder.put("cdeadlines", currentUser.createdDeadlinesToMap());
+			builder.put("errors", "undefined");
+			builder.put("cgroups", currentUser.groupsToMap());
+			builder.put("url", url);
+			
+			return builder.build();
 		}
 		
 		log.debug("User is not authorised to view supervision homepage, redirecting");
